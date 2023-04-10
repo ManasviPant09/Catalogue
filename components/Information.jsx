e5ac8fc1dcdaf45b334b6cb10b33db35134f85fb
Manasvi Pant <manasvipant1106@gmail.com>
2023-04-10 23:54:18 +0530

    Updated

--- components/Information.jsx
+++ components/Information.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
-
+import { useState } from 'react';
 const Information = () =>{
+  const [isOpen, setIsOpen] = useState(false);
+  function toggleDropdown() {
+    setIsOpen(!isOpen);}
+    
   return(
     <div className="flex flex-col">
       <div className="p-4 border-b-2 border-gray-200 mt-0.5">
@@ -13,12 +17,12 @@ const Information = () =>{
             <input 
             className="w-96 h-12 px-3 pr-10 text-sm text-gray-700 placeholder-gray-400 border-2 border-gray-500 rounded-md focus:outline-gray focus:shadow-outline"
             type="text"/>
-            <div className="absolute inset-y-0 right-0 flex items-center" style={{marginRight: "4rem"}}>
-                <div className="rounded-md bg-orange-500 pt-3 pb-3 pr-3 pl-2 hover:bg-orange-700"style={{width: "3.5rem", marginLeft: "12rem"}}>
+            <div className="absolute inset-y-0 right-0 flex items-center mr-28">
+                <div className="rounded-md bg-orange-500 pt-3 pb-3 pr-3 pl-2 hover:bg-orange-700 w-14">
                     <svg
-                      className="w-6 h-6 fill-current text-white"
+                      className="w-6 h-6 fill-current text-white ml-1"
                       viewBox="-5 1 20 14"
-                      style={{marginLeft: "0.25rem"}}>
+                    >
                     <path
                       fillRule="evenodd"
                       d="M10.5 9h-.79l-.28-.27A6.47 6.47 0 0 0 11 4.5 6.5 6.5 0 1 0 4.5 11c1.71 0 3.26-.66 4.43-1.76l.27.28v.79l4 4L15 14l-4-4zm-6 0A4.5 4.5 0 1 1 9 4.5a4.5 4.5 0 0 1-4.5 4.5z"/>
@@ -27,8 +31,23 @@ const Information = () =>{
             </div>
         </div>
     <div className="text-white mt-2 mb-6 w-96">Use the Advanced Search to explore Pokémon by type, weakness, ability and more!</div></div>
-    <h1 className="rounded-lg h-24 text-xl text-white mt-8 mr-48 bg-green-500 p-4" style={{width: "60rem"}}>Search for a Pokémon by name or using its National Pokédex Number.</h1></div>    
+    <h1 className="rounded-lg h-28 text-xl text-white mt-8 mr-48 bg-green-500 p-4 w-auto">Search for a Pokémon by name or using its National Pokédex Number.</h1></div>    
     <div className="h-16 bg-gray-400">
+    </div>
+    <div className="flex justify-center">
+        <button className="bg-blue-400 hover:bg-blue-600 text-white font-medium py-4 px-32 rounded">Surprise Me</button>
+        <div className={`${isOpen ? 'block' : 'hidden'} absolute right-0`}>
+        <div className="dropdown">
+          <div tabIndex="0" className="m-1 btn btn-sm btn-outline-primary dropdown-toggle" onClick={toggleDropdown}>
+            Options
+          </div>
+          <div tabIndex="-1" role="menu" aria-hidden="true" className="menu dropdown-menu">
+            <div tabIndex="0" role="menuitem" className="menu-item">Option 1</div>
+            <div tabIndex="0" role="menuitem" className="menu-item">Option 2</div>
+            <div tabIndex="0" role="menuitem" className="menu-item">Option 3</div>
+          </div>
+        </div>
+      </div>
     </div>
 </div>
 );
