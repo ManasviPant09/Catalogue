8d4196233c59c1e1616ff3306191b049a5547153
Manasvi Pant <manasvipant1106@gmail.com>
2023-04-11 18:06:42 +0530

    Updated

--- components/Information.jsx
+++ components/Information.jsx
@@ -64,10 +64,10 @@ const Information = () =>{
         <div className="relative inline-block text-left">
           <div className="flex justify-center items-center">
             <h3 className="flex justify-center text-lg font-medium text-gray-700 mr-6">Sort By</h3>
-            <span className="rounded-md shadow-sm">
-              <button
+            <span className="rounded-md shadow-sm relative">
+            <button
                type="button"
-               className="mr-4 bg-gray-600 inline-flex justify-center w-full rounded-md border border-gray-300 px-12 py-2 bg-white text-medium font-medium text-white focus:outline-none focus:ring-indigo-500"
+               className="inline-flex items-center justify-center w-full rounded-md border border-gray-300 bg-gray-600 px-4 py-2 text-sm font-medium text-white hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white"
                id="options-menu"
                style={{minWidth: "18rem"}}
                aria-haspopup="true"
