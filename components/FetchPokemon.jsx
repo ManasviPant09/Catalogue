f75ad615447028397e79755d2cdd5ad2eef93bc2
Manasvi Pant <manasvipant1106@gmail.com>
2023-04-12 19:25:37 +0530

    Updated

--- components/FetchPokemon.jsx
+++ components/FetchPokemon.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { useQuery } from '@apollo/client';
 import { useRouter } from 'next/router';
 import { gql } from 'graphql-tag';
@@ -22,6 +22,28 @@ const POKEMONS_QUERY = gql`
       resistant
       weaknesses
       image
+      evolutions{
+        ...RecursivePokemonFragment
+      }
+    }
+  }
+  
+  fragment RecursivePokemonFragment on Pokemon {
+    id
+    name
+    number
+    image
+    types
+    maxCP
+    maxHP
+    evolutions {
+      id
+      name
+      number
+      image
+      types
+      maxCP
+      maxHP
     }
   }
 `;
@@ -32,16 +54,28 @@ const FetchPokemon = () => {
   const { loading, error, data } = useQuery(POKEMONS_QUERY, {
     variables: { id, name },
   });
-
+  const [showEvolution, setShowEvolution] = useState(false);
   if (loading) return <p>Loading</p>;
   if (error) return <p>Error</p>;
-
+  
   const { pokemon } = data;
-
+  const renderEvolution = (evolution) => {
+    return (
+      <div key={evolution.id} className="flex flex-col items-center">
+        <img src={evolution.image} alt={evolution.name} className="h-24" />
+        <p className="text-center">{evolution.name} #{evolution.number}</p>
+        {evolution.evolutions && (
+          <div className="flex">
+            {evolution.evolutions.map((e) => renderEvolution(e))}
+          </div>
+        )}
+      </div>
+    );
+  };
   return (
     <>
-      <div className="flex justify-center h-24 -mt-12 w-full bg-gray-600"></div>
-      <div className="flex flex-col justify-center w-112 bg-gray-300">
+      <div className="flex h-24 -mt-12 w-full bg-gray-600"></div>
+      <div className="flex flex-col justify-center items-center w-112 bg-gray-300">
         <h1 className="flex justify-center text-3xl text-gray-700 font-semibold mt-4 mb-6">{pokemon.name}&#xa0;&#xa0;#0{pokemon.number}</h1>
         <div className="flex justify-center mt-4">
         <img
@@ -64,6 +98,18 @@ const FetchPokemon = () => {
         </div>
         </div>
         </div>
+        <button onClick={() => setShowEvolution(!showEvolution)} 
+        className="flex justify-center items-center bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-2 rounded rounded-lg w-32">Show Evolution</button>
+        {showEvolution && (
+        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center z-10">
+            <div className="bg-white p-8 rounded-lg">
+                {pokemon.evolutions && renderEvolution(pokemon)}
+                <button onClick={() => setShowEvolution(false)}
+                className="bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded mt-4">
+                Close</button>
+            </div>
+        </div>
+        )}
       </div>
     </>
   );
